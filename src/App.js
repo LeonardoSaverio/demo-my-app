89b4f7cb562f7e22931db03351c618f74f58caf1
LeonardoSaverio <leonardosaverio@hotmail.com>
2021-10-26 17:55:25 -0300

    test bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+  <Button variant="light">Light</Button>
+   <Button variant="dark">Dark</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
